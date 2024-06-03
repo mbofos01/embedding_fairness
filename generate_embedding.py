7e62ed3ffa8bd7fd0da4a271cf8bbe4f06c5d3f3
Michail-Panagiotis Bofos <mbofos@outlook.com>
2024-06-03 17:30:29 +0200

    Generate Embedding implemented
    * Now we can actually generate and embedding and store it in a pickle file to reduce API calls

---
+++ generate_embedding.py
@@ -0,0 +1,39 @@
+from openai import OpenAI
+import pickle
+import os
+import read_embedding
+from typing import Tuple, List
+
+client = OpenAI()
+
+
+def generate_embedding(model_used: str = "text-embedding-3-large", sentence: str = "The doctor called his friend", save_to_file: bool = True, force_recompute: bool = False) -> Tuple[str, List[float]]:
+    """
+    Generate an embedding for a given sentence using a specified model.
+
+    Args:
+        model_used (str): The name of the model to be used for generating the embedding. Default is "text-embedding-3-large".
+        sentence (str): The input sentence for which the embedding needs to be generated. Default is "The doctor called his friend".
+        save_to_file (bool): Whether to save the generated embedding to a file. Default is True.
+        force_recompute (bool): Whether to force recomputation of the embedding even if it already exists in the "embeddings" directory. Default is False.
+
+    Returns:
+        tuple[str, list[float]]: A tuple containing the input sentence and the generated embedding as a list of floats.
+
+    """
+    if f"{sentence}.pkl" in os.listdir("embeddings") and not force_recompute:
+        return read_embedding.read_embedding(sentence)
+
+    generated_embedding = client.embeddings.create(
+        model=model_used, input=[sentence])
+
+    if save_to_file:
+        with open(f"embeddings/{sentence}.pkl", 'wb') as f:
+            pickle.dump(generated_embedding, f)
+
+    return (sentence, generated_embedding.data[0].embedding)
+
+
+if __name__ == '__main__':
+    generate_embedding(
+        "The doctor called his friend", save_to_file=True)
